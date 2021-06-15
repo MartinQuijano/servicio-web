28a8e0d03bec8af3830f0df740d0b9a6742541a3
martinquijano <mnquijanoalt@gmail.com>
2021-06-15 18:03:24 -0300

    Integracion swagger

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
 const express = require('express')
 const app = express()
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // routes
 app.use(require('./src/api/routes'));
